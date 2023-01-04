dde24558c4f58ca207c9c0316ffa7af45c2c6b7a
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-01-04 23:16:13 +0900

    updated

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const fav = document.getElementById('fav');
+const toRed = () => {
+    fav.setAttribute('href', "img/red.png");
+};
+const toGreen = () => {
+    fav.setAttribute('href', "img/green.png");
+};
+
